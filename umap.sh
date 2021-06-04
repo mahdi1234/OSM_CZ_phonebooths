b85ee713026f607959c8ea57b8c0892c5c8c2834
mahdi1234 <mahdi1234@centrum.cz>
2021-06-04 09:28:28 +0000

    Update umap.sh

--- umap.sh
+++ umap.sh
@@ -7,6 +7,7 @@ echo "Phone booths"
 #out center qt;
 wget -O all_phone_booths.json 'https://overpass-api.de/api/interpreter?data=%5Bout%3Ajson%5D%5Btimeout%3A200%5D%3Barea%283600051684%29%2D%3E%2EsearchArea%3Bnwr%5B%22amenity%22%3D%22telephone%22%5D%28area%2EsearchArea%29%3Bout%20qt%20center%3B%0A'
 
+sleep 300
 echo "Disued phone booths"
 #[out:json][timeout:200];
 #{{geocodeArea:Czechia}}->.searchArea;
