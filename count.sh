ad141adadef25e208c8955711a7943b99d88a972
mahdi1234 <mahdi1234@centrum.cz>
2021-06-04 09:27:12 +0000

    Update count.sh

--- count.sh
+++ count.sh
@@ -2,3 +2,5 @@
 > ./progress.txt
 echo "Telefonni budky" >> ./progress.txt
 cat ./all_phone_booths.json | grep '"amenity": "telephone"' | wc -l >> ./progress.txt
+echo "disused: Telefonni budky" >> ./progress.txt
+cat ./disused_phone_booths.json | grep '"disused:amenity": "telephone"' | wc -l >> ./progress.txt
