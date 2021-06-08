5595f49b32d67d25c393ca441a53681c3b45f81b
mahdi1234 <mahdi1234@centrum.cz>
2021-06-08 09:11:15 +0000

    Update count.sh

--- count.sh
+++ count.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 > ./progress.txt
 echo "Telefonni budky" >> ./progress.txt
-active=$(cat ./active_phone_booths.xml | grep 'k="amenity" v="telephone"' | wc -l)
+active=$(cat ./all_phone_booths.xml | grep 'k="amenity" v="telephone"' | wc -l)
 echo $active >> ./progress.txt
 echo "disused: Telefonni budky" >> ./progress.txt
-disused=$(cat ./disused_phone_booths.xml | grep 'k="disused:amenity" v="telephone"' | wc -l)
+disused=$(cat ./disused_phone_booths.xml | grep -e 'k="disused:amenity" v="telephone"' -e 'k="historic:amenity" v="telephone"' -e 'k="was:amenity" v="telephone"' -e 'k="abandoned:amenity" v="telephone"' -e 'k="removed:amenity" v="telephone"' -e 'k="amenity:disused" v="telephone"' | wc -l)
 echo $disused >> ./progress.txt
 total=$(( $active + $disused ))
 echo "Celkem - $total" >> ./progress.txt
