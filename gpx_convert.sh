04f35bc7c548b8d58d51d0ddc4408d4ab27fa218
mahdi1234 <mahdi1234@centrum.cz>
2021-07-26 21:35:14 +0000

    Update gpx_convert.sh
    Remove time from gpx to prevent push when nothing changed

--- gpx_convert.sh
+++ gpx_convert.sh
@@ -3,3 +3,4 @@
 gpsbabel -i osm -f active_phone_booths.xml -o gpx -F active_phone_booths.gpx
 gpsbabel -i osm -f disused_phone_booths.xml -o gpx -F disused_phone_booths.gpx
 gpsbabel -i osm -f verified_phone_booths.xml -o gpx -F verified_phone_booths.gpx
+sed -i '/.*time.*/d' *.gpx
